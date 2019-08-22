ed93fb318610ac7ca10fdb6dfea9773c72ae9728
tanweer anwar <tanweer.anwar919@gmail.com>
2019-08-22 22:57:15 +0530

    Added PWA capability

---
+++ serviceWorker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {});
+self.addEventListener('activate', function(event) {
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
